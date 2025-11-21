2b2397d0df8595cebba894e78c3e2a0bb577903d
Ykk2001 <yogeshkhatake18@gmail.com>
2025-11-21 23:34:37 +0530

    DONE

---
+++ src/components/TodoList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+export default function TodoList({todos,handleComplete}) {
+  return (
+    <div>
+      <h2>Child Component</h2>
+      <ul>
+        {
+          todos.map((todo)=>{
+          return ( <li key={todo.id}>
+
+              {todo.text}{" "}
+              {
+                !todo.completed && <button onClick={()=>handleComplete(todo.id)}>Complete</button>
+              }
+              
+            </li>)
+          })
+        }
+      </ul>
+    </div>
+  )
+}
